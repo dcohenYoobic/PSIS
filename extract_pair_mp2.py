a17abc05addb0e829f164c8526f07b38b5d6585d
Ubuntu <ubuntu@tf-dcohen.c.deeplearning-159513.internal>
2020-03-09 08:50:29 +0000

    extract pair in python with multiprocessing

---
+++ extract_pair_mp2.py
@@ -0,0 +1,132 @@
+from pycocotools.coco import COCO
+import skimage.io as io
+import cv2
+import json
+import os
+import multiprocessing
+import functools
+from joblib import Parallel, delayed
+from functools import partial
+import signal
+import time
+from PIL import Image, ImageFilter,ImageEnhance,ImageOps,ImageFile
+import numpy as np
+import pandas as pd 
+from tqdm import tqdm
+def PIL2array1C(img):
+    '''Converts a PIL image to NumPy Array
+
+    Args:
+        img(PIL Image): Input PIL image
+    Returns:
+        NumPy Array: Converted image
+    '''
+    return np.array(img.getdata(),
+                    np.uint8).reshape(img.size[1], img.size[0])
+
+
+
+def image_generation(ref_image_id, ref_id,ref_mask_area, ref_instance_size, ref_xmax, ref_ymax, ref_xmin, ref_ymin,
+                    ref_mask_array, sec_file):
+    category_name = "box"
+    #ann2ann_list = []
+    ann2ann={}
+    pro_id = str(sec_file.split('.')[0])
+    pro_image_id = int(ann2img[pro_id])
+    if ref_image_id==pro_image_id: return None
+    file_name = sec_file.split('.')[0]+'.pbm'
+    mask = Image.open(os.path.join(INSTANCE_FILE,category_name,file_name))
+    x,y,w,h = annotations[pro_image_id]['bbox']
+    xmin = int(round(x))
+    ymin = int(round(y))
+    xmax = int(round(x + w))
+    ymax = int(round(y + h))
+    instance_size = (xmax - xmin) * (ymax - ymin)
+    if instance_size != 0:
+        ref2pro_ratio = float(ref_instance_size) / float(instance_size)
+    else: ref2pro_ratio=0
+    if xmax == xmin or ymax == ymin : return None
+    mask = mask.crop((xmin,ymin,xmax,ymax))
+    mask = mask.resize(((ref_xmax-ref_xmin),(ref_ymax-ref_ymin)), Image.ANTIALIAS)
+    mask_array = 255-PIL2array1C(mask)
+    mask_area = np.sum(mask_array==255)
+    ssd = np.sum((ref_mask_array-mask_array)**2)
+    if ref_mask_area != 0: ref_ssd2instance_ratio = float(ssd) / float(ref_mask_area)
+    else: ref_ssd2instance_ratio = 1
+    if mask_area != 0: pro_ssd2instance_ratio = float(ssd) / float(mask_area)
+    else: pro_ssd2instance_ratio = 1
+    if ref_ssd2instance_ratio > 0.3 or pro_ssd2instance_ratio > 0.3 : return None
+    if ref2pro_ratio > 3 or ref2pro_ratio < 0.3: return None
+    if ssd == 0 : return None
+    
+    ann2ann[ref_image_id]=pro_image_id
+    #ann2ann_list.append(ann2ann)
+    #print('Finding satisfactory Annotation_a %s and Annotation_b %s for class %s'%(ref_id,pro_id,category_name))
+    return ann2ann
+
+    
+INVERTED_MASK = 1
+INSTANCE_FILE = "/home/ubuntu/mask_box/"#MSCOCO instance mask output file path, e.g.'MSCOCO/masks/'
+ANNOTATION_FILE = "/home/ubuntu/output_psis"#annotation pair file path for each category,e.g.'MSCOCO/PSIS/'
+JSON_FILE= "/home/ubuntu/annotations.json"#MSCOCO anntation json file path,e.g.'MSCOCO/annotations/instances_train2017.json'
+MAPPING= "/home/ubuntu/mapping.json"
+try:
+    os.makedirs(ANNOTATION_FILE)
+except:
+    pass
+with open(JSON_FILE, 'r') as f:
+    dataset_src = json.load(f)
+with open(MAPPING, 'r') as f:
+    ann2img = json.load(f) 
+annotations = {annotation['index_unique']:annotation for annotation in dataset_src['annotations']}
+
+category_list = ['box']
+#multi_process()
+#single_process()
+from itertools import combinations,product
+category_name = "box"
+print('Category_name is %s'%(category_name))
+for dirpath,dirnames,filenames in tqdm(os.walk(os.path.join(INSTANCE_FILE,category_name))):
+    pass
+list_results = []
+f=open(os.path.join(ANNOTATION_FILE,category_name,'ann2ann.txt'),'a')
+for ind, first_file in enumerate(tqdm(filenames)):
+    file_name = first_file.split('.')[0]+'.pbm'
+    ref_id = str(first_file.split('.')[0])
+    ref_image_id = int(ann2img[ref_id])
+    ref_mask = Image.open(os.path.join(INSTANCE_FILE,category_name,file_name))
+    ref_x,ref_y,ref_w,ref_h = annotations[ref_image_id]['bbox']
+    ref_xmin = int(round(ref_x))
+    ref_ymin = int(round(ref_y))
+    ref_xmax = int(round(ref_x + ref_w))
+    ref_ymax = int(round(ref_y + ref_h))
+    ref_instance_size = (ref_xmax - ref_xmin) * (ref_ymax - ref_ymin)
+    if ref_xmax == ref_xmin or ref_ymax == ref_ymin : contimue
+    ref_mask = ref_mask.crop((ref_xmin,ref_ymin,ref_xmax,ref_ymax))
+    ref_mask_array = 255-PIL2array1C(ref_mask)
+    ref_mask_area = np.sum(ref_mask_array==255)
+    #image_gen = lambda sec_file: image_generation(ref_image_id, ref_id,ref_mask_area, ref_instance_size, ref_xmax, ref_ymax, ref_xmin, ref_ymin,ref_mask_array, sec_file)
+    #image_gen(filenames[0])
+    results = Parallel(n_jobs = -1, verbose = 10, backend = "multiprocessing")(delayed(image_generation)(ref_image_id, ref_id,ref_mask_area, ref_instance_size, ref_xmax, ref_ymax, ref_xmin, ref_ymin,
+                    ref_mask_array, sec_file)for sec_file in filenames[ind:])
+    temp_res = [x for x in results if x is not None]
+    for ann2ann in temp_res:
+        f.write(str(ann2ann)+'\n')
+
+#file_names = filenames
+#paired = list(product(filenames, filenames))
+#df = pd.DataFrame({"filename":filenames})
+#df2 = pd.DataFrame({"filename":filenames})
+#print(df.head())
+#print(len(df))
+#print(len(df2.join(df, how = "outer", on = "filename")))
+#couple_images = set(map(frozenset, combinations(set(filenames), 2)))
+#print(couple_images[0:10])
+#image_generation(category_list[0])
+#results = Parallel(n_jobs = -1, verbose = 10, backend = "multiprocessing")(delayed(image_generation)(i,j) for i,j in product(filenames, filenames))
+print("Done")
+#for k in list_results:
+#for ann2ann in k:
+#f.write(str(ann2ann)+'\n')
+f.close()
+print('Master: I\'m Done')
